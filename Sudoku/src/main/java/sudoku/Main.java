57dce55173d7f120d6dd8b2874ad3dce207eb97b
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-11-19 23:05:03 +0200

    javafx 11 jar fix

---
+++ Sudoku/src/main/java/sudoku/Main.java
@@ -0,0 +1,14 @@
+package sudoku;
+
+import sudoku.ui.SudokuUi;
+
+/**
+ *
+ * @author hoffrenm
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SudokuUi.main(args);
+    }
+}
