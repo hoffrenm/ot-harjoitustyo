0592153682b2f0e6ae41f2654b5aaab45da0e3f5
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-11-26 23:09:56 +0200

    helper now serves seemingly random boards, finish rule checking methods

--- Sudoku/src/main/java/sudoku/logics/BoardHelper.java
+++ Sudoku/src/main/java/sudoku/logics/BoardHelper.java
@@ -1,5 +1,7 @@
 package sudoku.logics;
 
+import java.util.ArrayList;
+import java.util.Random;
 import sudoku.domain.Board;
 import sudoku.domain.Cell;
 
@@ -9,20 +11,47 @@ import sudoku.domain.Cell;
  */
 public class BoardHelper {
     
-    public boolean numberCanBeInserted(Board board, Cell cell, int value) {
-        return true;
+    private String[] sudokus = {"176328459538149672492765138657834921924651387381972564813296745749583216265417893", 
+                                "278634951935271486416859237743915628681723549529486173394568712152397864867142395",
+                                "312654978754982316968713425586379241291465837473128659625837194147296583839541762",
+                                "586413729732569148941278356429831567865724913173956284298147635314695872657382491",
+                                "428971653596238741317546892143859267982617534675324189864792315239185476751463928"};
+    
+    private String testSudoku = "030200570000300040901047008002003087080600000056010239800020400400098005017005006";
+    
+    // Atleast user thinks sudokus are generated randomly
+    // Todo: make them actually random with varying difficulties
+    public void initializeBoard(Board board, int numberOfCells) {
+        Random random = new Random();
+        
+        String sudoku = sudokus[random.nextInt(sudokus.length)];
+        
+        for (int i = 0; i < numberOfCells; i++) {
+            int row = random.nextInt(9);
+            int col = random.nextInt(9);
+            
+            int index = col + row * 9;
+            
+            board.getCells().get(index).setValue(Character.getNumericValue(sudoku.charAt(index)));   
+        }
+    }
+    
+    public void initializeTestSudoku(Board board) {
+        for (int i = 0; i < testSudoku.length(); i++) {
+            board.getCells().get(i).setValue(Character.getNumericValue(testSudoku.charAt(i)));   
+        } 
     }
     
     public boolean canBeInsertedToRow(Board board, Cell cell, int value) {
         return board.getCells().stream().filter(c -> c.getRow() == cell.getRow()).noneMatch(c -> c.getValue() == value);
     }
-    
+
     public boolean canBeInsertedToColumn(Board board, Cell cell, int value) {
-        return true;
+        return board.getCells().stream().filter(c -> c.getColumn() == cell.getColumn()).noneMatch(c -> c.getValue() == value);
     }
-    
+
     public boolean canBeInsertedToSubgrid(Board board, Cell cell, int value) {
-        return true;
+        return board.getCells().stream().filter(c -> c.getSubgrid() == cell.getSubgrid()).noneMatch(c -> c.getValue() == value);
     }
-    
+
 }
