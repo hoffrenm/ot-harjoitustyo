0ddb6846623506cb5ce0425c09fdf7977ec77ef6
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-12-03 20:01:50 +0200

    create Timer to show playtime

---
+++ Sudoku/src/main/java/sudoku/ui/Timer.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku.ui;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+
+/**
+ *
+ * @author Mika Hoffren
+ */
+public class Timer {
+
+    private Timeline timeline;
+    private long seconds;
+    private long minutes;
+    private Label label;
+
+    public Timer() {
+        this.timeline = new Timeline();
+        this.seconds = 0;
+        this.minutes = 0;
+        this.label = new Label("Time: 00:00");
+        label.setFont(new Font("Arial", 30));
+        label.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        init();
+    }
+
+    private void init() {
+        timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                        e -> incrementTime()),
+                new KeyFrame(Duration.seconds(1)));
+
+        timeline.setCycleCount(Animation.INDEFINITE);
+    }
+
+    private void incrementTime() {
+        if (seconds < 59) {
+            seconds++;
+        } else {
+            seconds = 0;
+            minutes++;
+        }
+        
+        this.label.setText("Time " + minutes + ":" + seconds);
+    }
+    
+    public void start() {
+        timeline.play();
+    }
+    public void stop() {
+        timeline.stop();
+    }
+
+    public void reset() {
+        this.seconds = 0;
+        this.minutes = 0;
+    }
+
+    public Label getTimer() {
+        return this.label;
+    }
+
+}
