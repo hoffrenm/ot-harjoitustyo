8a4ad63b77501c3feda52b1144c7ceb8cc470221
hoffrenm <46321023+hoffrenm@users.noreply.github.com>
2019-12-20 17:29:22 +0200

    add tests for FileScore

---
+++ Sudoku/src/test/java/dao/FileScoreTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import sudoku.dao.FileScore;
+import static org.junit.Assert.*;
+import sudoku.domain.Score;
+
+/**
+ *
+ * @author Mika Hoffren
+ */
+public class FileScoreTest {
+
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    public File testFile;
+    public FileScore fileScore;
+
+    @Before
+    public void setUp() throws Exception {
+        testFile = temporaryFolder.newFile("testScores.txt");
+
+        try {
+            FileWriter fw = new FileWriter(testFile.getAbsolutePath());
+            fw.write("chimpanzee;12:34;easy\n");
+            fw.write("toad;0:45;hard\n");
+            fw.write("unicorn;2:04;medium");
+            
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        fileScore = new FileScore(testFile.getAbsolutePath());
+    }
+
+    @Test
+    public void canBeInitializedWithExistingFile() {
+        assertNotNull(fileScore);
+    }
+
+    @Test
+    public void scoresFromFileAreStoredToList() {
+        System.out.println(fileScore.getScores().size());
+        assertEquals(3, fileScore.getScores().size());
+    }
+    
+    @Test
+    public void scoresAreSavedToFile() {
+        fileScore.getScores().add(new Score("monkey", "40:52,", "easy"));
+        fileScore.getScores().add(new Score("donkey", "2:32,", "easy"));
+        fileScore.getScores().add(new Score("dog", "5:41,", "easy"));
+        fileScore.getScores().add(new Score("cat", "0:11,", "easy"));
+        fileScore.save(fileScore.getScores());
+        
+        fileScore = new FileScore(testFile.getAbsolutePath());
+        
+        assertEquals(7, fileScore.getScores().size());
+    }
+
+    @After
+    public void tearDown() {
+        testFile.delete();
+    }
+
+}
